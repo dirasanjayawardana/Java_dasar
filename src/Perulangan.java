3296f89f573d862c777cd8a661fabe5e05926722
dirasanjayawardana <dirawardana.dw@gmail.com>
2023-12-15 22:54:03 +0700

    update

--- src/Perulangan.java
+++ src/Perulangan.java
@@ -4,5 +4,19 @@ public class Perulangan {
         for (int i=0; i<=10; i++){
             System.out.println("looping for ke-" + i);
         }
+
+        //------ looping while ------//
+        var counter = 1;
+        while (counter <= 10){
+            System.out.println("Looping while ke-" + counter);
+            counter++;
+        }
+
+        //------ do while loop (perulangan dilakukan baru pengecekan - jadi ada minimal 1 kali perulangan) ------//
+        var parameter = 1;
+        do {
+            System.out.println("Lopping do while ke-" + parameter);
+            parameter ++;
+        } while(parameter <= 10);
     }
 }
